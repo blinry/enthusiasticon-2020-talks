feb2fab5dd9c1b4edd89bec25082a55c7a881174
Sebastian Morr <sebastian@morr.cc>
2020-07-13 22:09:41 +0200

    Timestamps and descriptions after the lunch break

--- render.rb
+++ render.rb
@@ -54,56 +54,78 @@ In this talk, I'll introduce you to what gamma correction is, and how knowing ab
   {
     speakers: "Nikki Stevens",
     title: "Keynote",
-    start: "0:00:00",
-    end: "0:00:00",
+    start: "4:05:31",
+    end: "4:49:18",
+    description: "In this keynote, Nikki will discuss the relationships between political activism and our responsibilities as producers of technological systems. Informed by their dissertation research, the keynote will cover some common approaches to \"doing good with tech\" and some ways we can move forward to think more ethically and inclusively about the technology we're building and consuming.",
   },
   {
     speakers: "Lisa Passing",
     title: "Build your own controller!",
-    start: "0:00:00",
-    end: "0:00:00",
+    start: "5:01:31",
+    end: "5:16:42",
+    description: "When we play games we do so with a keyboard and mouse, a touch display or gamepad. But why limit ourselves to these options? We can build game controllers using simple electronics parts and create unique ways of interacting with our games!",
   },
   {
     speakers: "Ilo_ul",
     title: "A “How-To” for writing a “How-To” for Open Source Software",
-    start: "0:00:00",
-    end: "0:00:00",
+    start: "5:18:24",
+    end: "5:30:44",
+    description: "Open Source Software is important, Open Source Software is the best, Open Source Software is easily accessible and still: Why isn’t everyone using it?
+I want to argue that very often Open Source Software (or software in general) lacks accessibility for non-nerdy people because they seem very “complicated”. Even the “how-to” instructions do with the love for technical terms, mystical coding-symbols and pragmatic on-point language.
+
+In April 2020 I decided if everyone is using videocalls now, so will my grandfather do. So I got a tablet, sat down and tried to explore it like I had never used a tablet before. What could be hick-ups, how could I accidentally end up in some sub-menu?
+What I ended up with is a 20+ pages long instruction with screenshots from switching on the tablet, to muting the microphone or unlocking the screen (and much more).
+I want to share with you the manual I created but moreover I want to share what I learned when I first used it with my grandfather. (And also what I had to change because I was designing it for him without consulting him)
+Spoiler: He is successfully using Jitsi! (Without being stressed.)",
   },
   {
     speakers: "Vicky Twomey-Lee",
     title: "My First Maker Project: Print Your Own Adventure Game",
-    start: "0:00:00",
-    end: "0:00:00",
+    start: "5:31:20",
+    end: "5:41:55",
+    description: "A novice delving into the world of electronics, I will take you on a quick journey on how I created this Maker project destined for a Makerfaire this year. But alas it was cancelled, however I did complete the project made with a Raspberry Pi, a thermal receipt printer, and a few colourful arcade buttons. As a Pythonista more comfortable with coding, I controlled it all with CircuitPython.",
   },
   {
     speakers: "Tamas Kadar",
     title: "Little Printing for everyone!!1",
-    start: "0:00:00",
-    end: "0:00:00",
+    start: "5:42:29",
+    end: "5:52:46",
+    description: "Do you remember the Little Printer? It was an internet-connected, super cute thermal printer; it printed you the weather in the morning, and you could even message your friends if they also had one. It was a commercial flop and the servers were shut down in 2015, but that was not the end of the story. Today, with a little work, you too have your own Little Printer — and now you can use any thermal printer for it (some assembly may be required).",
   },
   {
     speakers: "Em Lazer-Walker",
     title: "So THAT’S how my phone knows where I am!",
     start: "0:00:00",
     end: "0:00:00",
+    description: "Your phone knows where you are at all times, usually with pretty shocking accuracy. How the heck does it know that?! This talk will dive into the math and physics underlying modern smartphone location technology (GPS, AGPS, and indoor location), but it will also do so by means of a walk through history. Modern GPS arose out of Cold War-era US DoD research, but how is that research connected to the larger historical and political context of geolocation throughout history? How is the technical solution to tracking Russian missiles directly indebted to the 17th century sailors who developed smarter ways to locate themselves on the open sea? This talk will answer all of that and more!",
   },
   {
     speakers: "bleeptrack",
     title: "Exploring Generative Spaces",
-    start: "0:00:00",
-    end: "0:00:00",
+    start: "6:22:46",
+    end: "6:33:08",
+    description: "In \"Exploring Generative Spaces\" we will wander through the results from various generative models. What makes a model interesting and how can we navigate in generative spaces? Do parametric and neural models behave the same? Join me in this playground of generative art.",
   },
   {
     speakers: "Serena Peruzzo and John Earnest",
     title: "The Meaning of K",
-    start: "0:00:00",
-    end: "0:00:00",
+    start: "6:33:52",
+    end: "6:44:00",
+    description: "Data Science methods are not normally written in K. But what if they were?
+
+K is a high-performance programming language popular in finance, but rarely seen elsewhere, and is known for its expressive and concise notation. While K is much less well known among data scientists than Python, it shares a common heritage with NumPy as both were strongly influenced by APL.
+
+KMeans is a classic machine learning algorithm which partitions the data into homogeneous groups (clusters). Its simplicity and scalability make it a popular choice for a broad range of data science applications, such as customer segmentation and recommendation engines.
+
+In this talk, we’ll walk you through a step by step implementation of KMeans in K and demonstrate how the features of this esoteric language encourage poetic expression of mathematical concepts and more intuitive interaction with data.",
   },
   {
     speakers: "Knut Hühne",
     title: "What's in a name? A gentle introduction to DNS",
-    start: "0:00:00",
-    end: "0:00:00",
+    start: "6:44:22",
+    end: "6:54:29",
+    description: "Whenever we use the internet, we use the Domain Name System - DNS. It works so well that it is mostly hidden from us and we as developers rarely have to look at it. If we do have to, it helps to understand how DNS is structured and how it works.
+In this talk I will give a gentle introduction to DNS by showing how a domain name is translated to an IP address step by step. We will follow it on its path and learn about some basic records - namely A, AAAA and NS. Going out of this talk, you should have an intuitive understanding of DNS and a good basis to debug most DNS issues in the future.",
   },
 ]
 
@@ -144,13 +166,14 @@ def cut_talk(talk, i)
   run "echo -e \"file tmp/title#{i}.mp4\nfile tmp/talkonly#{i}.mp4\nfile tmp/endingframe.mp4\nfile tmp/ending.mp4\" | ffmpeg -y -f concat -protocol_whitelist file,pipe,crypto -i - -c copy output/talk#{i}.mp4"
 end
 
-from = 0
-to = 1
+from = 16
+to = 16
 
 run "mkdir -p tmp output"
 prepare_the_ending
 
 talks[from..to].each_with_index do |talk, i|
+  i = from + i
   generate_youtube_text talk, i
   cut_talk talk, i
 end
